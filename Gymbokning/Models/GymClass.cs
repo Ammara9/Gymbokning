c2eba1c0a7a7f650a635cbe2f36204319b76a895
Ammara Shakeel <ammarashakeel81@yahoo.se>
2024-11-12 17:24:08 +0100

    constructors

--- Gymbokning/Models/GymClass.cs
+++ Gymbokning/Models/GymClass.cs
@@ -5,13 +5,14 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime StartTime { get; set; }
-
         public TimeSpan Duration { get; set; }
-        public DateTime EndTime
-        {
-            get { return StartTime + Duration; }
-        }
-
+        public DateTime EndTime => StartTime + Duration; // EndTime beräknas från StartTime och Duration
         public string Description { get; set; }
+
+        public GymClass(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
     }
 }
